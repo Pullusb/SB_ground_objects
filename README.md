1a89bb05970a110a6ccfbfb8da0a9a091d1794ae
Pullusb <bernou.samuel@gmail.com>
2020-09-21 10:02:54 +0200

    feat:center on axis

--- README.md
+++ README.md
@@ -10,17 +10,45 @@ Want to support ? [Check out how](http://www.samuelbernou.fr/donate)
 
 ### Description
 
-Super simple addon to put bottom of selected at 0 in Z axis with or without offset on the   selection.  
-Because sometimes you just want that!
+Super simple addon to put bottom of selected at 0 in Z axis (_ground objects_) with or without offset on the selection.  
+Because sometimes you just want that!  
+Also you can _center objects_ on chosen world axis.
 
-Probably reinventing the wheel here, but better have options ;)
+All of this is usefull to quickly place a model properly at the center of the scene for future linking.
+
+Probably reinventing the wheel here, but as they say : better have options ;)
 
 
-1.0.0 Demo
+Options:
 
-![ground object demo gif](https://raw.githubusercontent.com/Pullusb/images_repo/master/Bl_grd-obj_ground_demo_v1.gif)
+**Offset from active** : Consider only active object to move with all other objects following along (incompatible with individual mode)
+
+**Individually** : Affect objects in selection individually instead of considering as a group
+
+
+### 1.1.0 Demo:
+
+
+**Ground objects**  
+
+![ground object demo gif](https://raw.githubusercontent.com/Pullusb/images_repo/master/Bl_grd-obj_v2_grounding.gif)
+
+
+**Center on World axis**  
+
+![center axis demo gif](https://raw.githubusercontent.com/Pullusb/images_repo/master/Bl_grd-obj_v2_center_axis.gif)
+
+<!-- 1.0.0 Demo
+
+![ground object demo gif](https://raw.githubusercontent.com/Pullusb/images_repo/master/Bl_grd-obj_ground_demo_v1.gif) -->
 
 ## Changelog:
+
+1.1.0:
+
+- feat: offset from active option
+- feat: center (world) on specific axis
+- new UI
 
 1.0.0:
 
--- __init__.py
+++ __init__.py
@@ -19,7 +19,7 @@ bl_info = {
 "name": "Ground objects",
 "description": "Ground selected objects (Using lowest point of bounding box)",
 "author": "Samuel Bernou",
-"version": (1, 0, 0),
+"version": (1, 1, 0),
 "blender": (2, 83, 0),
 "location": "Sidebar > Tool > Ground object",
 "warning": "",
@@ -40,25 +40,32 @@ class MESH_OT_ground_object(bpy.types.Operator):
     """Ground all selected object keepong offset"""
     bl_idname = "object.ground_selected_objects"
     bl_label = "Ground all objects"
+    bl_options = {'REGISTER', 'UNDO'}
 
     @classmethod
     def poll(cls, context):
         return context.active_object is not None
 
-    all_to_zero : bpy.props.BoolProperty(
-        name="All to zero", description="", default=False)
+    # all_to_zero : bpy.props.BoolProperty(
+    #     name="All to zero", description="", default=False)
 
     def execute(self, context):
-        if self.all_to_zero:
+        settings = context.scene.grd_objs_toolsettings
+
+        if not settings.use_active and settings.affect_individually:
             for o in context.selected_objects:
                 to_zero(o)
             self.report({'INFO'}, f'Grounded {len(context.selected_objects)} objects')
             return {'FINISHED'}
 
         points = []
-        for o in context.selected_objects:
-            for b in o.bound_box:
-                points.append(o.matrix_world @ Vector(b))
+        if settings.use_active:# consider active object Bbox
+            points = [context.object.matrix_world @ Vector(b) for b in context.object.bound_box]
+
+        else:# All obj bbox
+            for o in context.selected_objects:
+                for b in o.bound_box:
+                    points.append(o.matrix_world @ Vector(b))
 
         points.sort(key=lambda x: x[2])
         z_altitude = points[0][2]
@@ -69,8 +76,64 @@ class MESH_OT_ground_object(bpy.types.Operator):
         self.report({'INFO'}, f'Z moved by {z_altitude}')#WARNING, ERROR
         return {'FINISHED'}
 
+
+class MESH_OT_center_on_axis(bpy.types.Operator):
+    """Put the model at center of defined axis"""
+    bl_idname = "object.center_on_axis"
+    bl_label = "Center on one axis"
+    bl_options = {'REGISTER', 'UNDO'}
+
+    @classmethod
+    def poll(cls, context):
+        return context.active_object is not None
+
+    individually : bpy.props.BoolProperty(
+        name="Individually", description="", default=False)
+
+
+    axis = bpy.props.IntProperty(default=0)
+
+        #(key, label, descr, id[, icon])
+    def to_center(self, o, axis=1):
+        '''If no axis is given, on Y axis'''
+        points = [o.matrix_world @ Vector(b) for b in o.bound_box]
+        points.sort(key=lambda x: x[axis])
+        # all course to zero minus half the distance between min and max (max - min)/2 (equivalent to o.dimensions[axis]/2)
+        # o.location[axis] -= points[0][axis] + (points[-1][axis] - points[0][axis])/2
+        o.location[axis] -= points[0][axis] + o.dimensions[axis]/2
+
+    def execute(self, context):
+        axis = self.axis
+        settings =  context.scene.grd_objs_toolsettings
+        if not settings.use_active and settings.affect_individually:
+            for o in context.selected_objects:
+                self.to_center(o, axis)
+            self.report({'INFO'}, f'Centered {len(context.selected_objects)} objects')
+            return {'FINISHED'}
+
+        points = []
+        if settings.use_active:# consider active object Bbox
+            points = [context.object.matrix_world @ Vector(b) for b in context.object.bound_box]
+
+        else:# consider all selected object Bbox
+            for o in context.selected_objects:
+                for b in o.bound_box:
+                    points.append(o.matrix_world @ Vector(b))
+
+
+        points.sort(key=lambda x: x[axis])
+        min_axis_dist = points[0][axis]
+        max_axis_dist = points[-1][axis]
+        offset = min_axis_dist + ((max_axis_dist - min_axis_dist)/2)
+
+        for o in context.selected_objects:
+            o.location[axis] -= offset
+
+        self.report({'INFO'}, f"Axis {['X','Y','Z'][axis]} moved by {offset}")
+        return {'FINISHED'}
+
 class MESH_PT_ground_object_UI(bpy.types.Panel):
-    bl_label = "Ground object"
+    bl_label = "Ground objects"
     bl_space_type = 'VIEW_3D'
     bl_region_type = 'UI'
     bl_category = 'Tool'
@@ -78,25 +141,71 @@ class MESH_PT_ground_object_UI(bpy.types.Panel):
 
     def draw(self, context):
         layout = self.layout
-        layout.operator("object.ground_selected_objects", text = 'Ground objects with offset').all_to_zero = False
-        layout.operator("object.ground_selected_objects", text = 'Ground all to zero').all_to_zero = True
+        # layout.use_property_split = True
 
+        ## Options
+        layout.prop(context.scene.grd_objs_toolsettings, 'use_active')
+
+        # if not context.scene.grd_objs_toolsettings.use_active:
+            # layout.prop(context.scene.grd_objs_toolsettings, 'affect_individually')
+        col = layout.column()
+        col.active = not context.scene.grd_objs_toolsettings.use_active
+        col.prop(context.scene.grd_objs_toolsettings, 'affect_individually')
+
+        ## Ground
+        layout.operator("object.ground_selected_objects", text = 'Ground objects')
+        # layout.operator("object.ground_selected_objects", text = 'Ground objects with offset').all_to_zero = False
+        # layout.operator("object.ground_selected_objects", text = 'Ground all to zero', icon='AXIS_TOP').all_to_zero = True
+        
+        # layout.separator()
+        
+        ## Center
+        ### commented:  layout.prop(context.scene, 'grd_object_center_axis')
+        # layout.operator("object.center_on_axis", text = 'Center on Axis').use_active = True
+        row = layout.row(align=True)
+        row.label(text='Center on')
+        row.operator("object.center_on_axis", text = 'X').axis = 0
+        row.operator("object.center_on_axis", text = 'Y').axis = 1
+        row.operator("object.center_on_axis", text = 'Z').axis = 2
+
+
+class GRD_PGT_toolsettings(bpy.types.PropertyGroup) :
+    affect_individually : bpy.props.BoolProperty(
+        name="Individually",
+        description="Affect objects in selection individually instead of considering as a group",
+        default=False)
+
+    use_active : bpy.props.BoolProperty(
+        name="Offset from active", description="Consider only active object to move with all other objects following along\n(incompatible with individual mode)", default=False)
+    
+    # bpy.types.Scene.grd_object_center_axis = bpy.props.EnumProperty(
+    #     name="Axis", description="Axis to align to", default='Y',
+    #     items=(
+    #         ('X', 'X', 'Center on X "lateral" axis', 'AXIS_SIDE', 0),
+    #         ('Y', 'Y', 'Center on Y "depth" axis', 'AXIS_FRONT', 1),
+    #         ('Z', 'Z', 'Center on Z "height" axis', 'AXIS_TOP', 2),
+    #         ))
 
 
 ### --- REGISTER ---
 classes = (
+GRD_PGT_toolsettings,
 MESH_OT_ground_object,
+MESH_OT_center_on_axis,
 MESH_PT_ground_object_UI,
 )
 
 def register():
     for cls in classes:
         bpy.utils.register_class(cls)
+    bpy.types.Scene.grd_objs_toolsettings = bpy.props.PointerProperty(type = GRD_PGT_toolsettings)
+
 
 def unregister():
     for cls in reversed(classes):
         bpy.utils.unregister_class(cls)
 
+    del bpy.types.Scene.grd_objs_toolsettings
 
 if __name__ == "__main__":
     register()
